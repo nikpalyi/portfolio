4780706b2938d8d4c0bf7afa36b5307a18676522
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 18:46:30 +0000

    Updates

---
+++ precache-manifest.787c8877b4c6fe840d0f239aff30294a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f4fe147713db65d8c0d648219e8206",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7efb7a7725fa7b74ada9",
+    "url": "/portfolio/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "442487a397cb69237538",
+    "url": "/portfolio/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "7efb7a7725fa7b74ada9",
+    "url": "/portfolio/static/js/2.aa1aac10.chunk.js"
+  },
+  {
+    "revision": "442487a397cb69237538",
+    "url": "/portfolio/static/js/main.7318cb30.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/portfolio/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/portfolio/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "/portfolio/static/media/youtube.0f423153.png"
+  }
+]);
