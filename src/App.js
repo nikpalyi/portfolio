7b89f0e05090fd4107cee9d2c0bef5edde822bee
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-31 13:32:04 +0000

    tile  edit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         { title: 'Contact', path: '/contact' }
       ],
       portfolio: {
-        title: 'portfolio',
+        title: 'projects',
         subTitle: 'React exercises',
         text: 'Checkout my side projects below'
       },
